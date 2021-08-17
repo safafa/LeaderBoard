6711fed699b1016d0ef5b5dbb9663e0da20a5d03
safafa <safaaballagh97@gmail.com>
2021-08-17 11:32:28 +0100

    Config webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+      contentBase: './dist',
+      port: '8081',
+  },
   plugins: [
    new HtmlWebpackPlugin({
     title: 'Output Management',
