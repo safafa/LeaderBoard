dfcc03390bb296890afe9253654f24a8552e52d7
safafa <safaaballagh97@gmail.com>
2021-08-31 16:09:05 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-import _ from 'lodash';
+import 'lodash';
 import './style.css';
 
 const players = [
- {
-   name: "Safa",
-   score: 100
- },
- {
-  name: "Safa",
-  score: 100
- },
- {
-  name: "Safa",
-  score: 100
- },
- {
-  name: "Safa",
-  score: 100
- },
-{
-  name: "Safa",
-  score: 100
-},
-{
-  name: "Safa",
-  score: 100
-},
-]
+  {
+    name: 'Safa',
+    score: 100,
+  },
+  {
+    name: 'Safa',
+    score: 100,
+  },
+  {
+    name: 'Safa',
+    score: 100,
+  },
+  {
+    name: 'Safa',
+    score: 100,
+  },
+  {
+  name: 'Safa',
+  score: 100,
+  },
+  {
+  name: 'Safa',
+  score: 100,
+  },
+];
 
-const renderScores = ()  => {
-    const list =  document.getElementById('scores-list');
-    players.forEach(player => {
-      const {name, score} = player;
-      const li = document.createElement('li');
-      li.innerText = `${name} : ${score}`;
-      list.appendChild(li);
-    })
-    return list;
-}
+const renderScores = () => {
+  const list =  document.getElementById('scores-list');
+    players.forEach((player) => {
+    const { name, score } = player;
+    const li = document.createElement('li');
+    li.innerText = `${name} : ${score}`;
+    list.appendChild(li);
+  });
+  return list;
+};
 
 renderScores();
