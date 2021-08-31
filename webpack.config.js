49ff339490c49ee08363aacfc503191fe3106727
safafa <safaaballagh97@gmail.com>
2021-08-31 16:04:42 +0100

    Fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+ /* eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -28,3 +29,4 @@ module.exports = {
     clean: true,
   },
 };
+ /* eslint-enable */
