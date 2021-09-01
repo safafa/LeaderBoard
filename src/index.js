dabff97d2c730af24b6ccd3b1b3a39c84c97cce7
safafa <safaaballagh97@gmail.com>
2021-09-01 11:46:32 +0100

    Add form eventlistner on submit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import { apiPost, apiGet } from './api.js';
 import { renderScores } from './render';
 
 const gameId = 'pTM1dfqEPrVz08MIv3ug';
+const from = document.getElementById('score-form');
+
+from.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  apiPost(`games/${gameId}/scores`, { 
+    "user": name,
+    "score": score
+  }).then((response) => {console.log(response)})
+})
 
 apiGet(`games/${gameId}/scores`).then(response => {
   renderScores(response.result);
--- src/render.js
+++ src/render.js
@@ -1,7 +1,7 @@
 export const renderScores = (players) => {
   const list = document.getElementById('scores-list');
   players.forEach((player) => {
-    const { name, score } = player;
+    const { user:name, score } = player;
     const li = document.createElement('li');
     li.innerText = `${name} : ${score}`;
     list.appendChild(li);
